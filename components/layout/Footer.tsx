cb590d6d509c49874fa715b07c3eac52c7f31628
kimoban <isaiahkimoban87@gmail.com>
2025-07-18 19:43:34 +0000

    chore: add .gitignore and remove node_modules from tracking

--- components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-6 mt-10">
-      <p>© 2025 ALX Listing. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
